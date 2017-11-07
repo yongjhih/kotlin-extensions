6c3d77b683b037c87e5e4ed7f69951f4f43123c1
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2017-11-07 14:32:21 +0100

    Add activity/fragment related extensions

---
+++ kotlinx-android-activity/src/main/kotlin/kotlinx/android/activity/ActivityExt.kt
@@ -0,0 +1,33 @@
+package kotlinx.android.activity
+
+import android.app.Activity
+import android.support.annotation.ColorRes
+import android.support.annotation.StringRes
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+
+fun Activity.toast(@StringRes resourceId: Int, length: Int = Toast.LENGTH_SHORT) {
+    toast(getString(resourceId), length)
+}
+
+fun Activity.toast(message: String, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, message, length).show()
+}
+
+fun Activity.colorFromResource(@ColorRes colorResource: Int): Int {
+    return ContextCompat.getColor(this, colorResource)
+}
+
+inline fun Activity.fragmentTransaction(function: android.app.FragmentTransaction.() -> android.app.FragmentTransaction) {
+    fragmentManager.beginTransaction()
+            .function()
+            .commit()
+}
+
+inline fun FragmentActivity.fragmentTransaction(function: FragmentTransaction.() -> FragmentTransaction) {
+    supportFragmentManager.beginTransaction()
+            .function()
+            .commit()
+}
